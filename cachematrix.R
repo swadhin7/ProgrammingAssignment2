d0eaf3518f0d1a0563f37ccdf6839d3f094ea76c
swadhin7 <43231898+swadhin7@users.noreply.github.com>
2018-11-02 14:03:01 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,33 @@
-makeCacheMatrix <- function(mtx = matrix()) {
-  inverse <- NULL
-  set <- function(x) {
-    mtx <<- x;
-    inverse <<- NULL;
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL 
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
   }
-  get <- function() return(mtx);
-  setinv <- function(inv) inverse <<- inv;
-  getinv <- function() return(inverse);
-  return(list(set = set, get = get, setinv = setinv, getinv = getinv))
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-cacheSolve <- function(mtx, ...) {
-  inverse <- mtx$getinv()
-  if(!is.null(inverse)) {
-    message("Getting cached data...")
-    return(inverse)
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
   }
-  data <- mtx$get()
-  invserse <- solve(data, ...)
-  mtx$setinv(inverse)
-  return(inverse)
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+#testing
+# m <- matrix(round(runif(9)*100), nrow = 3)
+# cm <- makeCacheMatrix(m)
+# cm$get()
+# cm
+# cacheSolve(cm)
+# solve(cm$get())
+# solve(m)
